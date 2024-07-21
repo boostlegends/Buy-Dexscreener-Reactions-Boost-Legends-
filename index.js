a71da75ed54b159181ecce6f1b23a51cf336663f
boostlegends <boostlegends1@gmail.com>
2024-07-21 10:36:48 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+// index.js
+const reactions = require('./main');
+
+console.log('Welcome to Buy Dexscreener Reactions Service!');
+console.log('Available Reactions:');
+console.log(reactions.getReactions());
+
+//Website: https://boostlegends.com/
